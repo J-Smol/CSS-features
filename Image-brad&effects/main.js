d7e69b67356ad11340a6ea2995d0bfe6c7135c48
Jane <jn.sm.new@gmail.com>
2019-04-09 13:27:18 +0400

    Add files via upload

---
+++ Image-brad&effects/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("img").load(function() {
+		$(this).wrap(function(){
+		return '<span class="image-wrap ' + $(this).attr('class') + '"style=""position:relative; display:inline-block;background:url(' + $(this).attr('src') + ') no-repeat center center;width: ' + $(this).width() + 'px;height: ' + $(this).height() + 'px;" />';
+		});
+	$(this).css("opacity","0");
+ 	});
+});
